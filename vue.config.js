9356e09ac1c894c547ab408a9f666b0cd71c8283
Arturo Medina <sirarthurnell@gmail.com>
2018-10-02 13:15:27 +0200

    Converted to an Electron app

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // configureWebpack: config => {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     config.output.publicPath = `${process.cwd()}/dist/`
+  //   }
+  // },
+  baseUrl: process.env.NODE_ENV === 'production'
+  ? '/'
+  : `${process.cwd()}/dist/`
+}
